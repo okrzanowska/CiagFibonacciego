cd7c9f1dfd4a8ef9287354c6779f45750a9121ea
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 14:16:45 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CiagFibonacciego
+{
+    class Program
+    {
+        private static int Fibonacci(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        private static void Main()
+        {
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+        }
+    }
+}
